a6ec98ac44cf515308d0bc2fd2d840bf748bd437
andreadgobbo@gmail.com <andreadgobbo@gmail.com>
2020-11-16 21:49:35 +0100

    Aggiungere i file di progetto.

---
+++ Calc1.0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Clasoperazioni;
+
+namespace Calc1._0
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double n1 = Class1.Num();
+            double n2 = Class1.Num();
+            double risulato = Class1.Num();
+            Console.Write($"{risulato}");
+
+        }
+    }
+}
---
+++ Operazioni/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clasoperazioni
+{
+    public class Class1
+    {
+        public static double Num()
+        {
+            Console.Write("inserire un numero->  ");
+            double n = double.Parse(Console.ReadLine());
+            return n;
+        }
+
+        public static double operazioni(double n1, double n2)
+        {
+            double risultato = 0;
+            Console.WriteLine("immettere: 1-somma,2-sottrazione,3-moltiplicazione,4-divisione");
+            Console.WriteLine("digitare operatore deciso");
+            string str = Console.ReadLine();
+            switch (str)
+            {
+                case "1":
+                case "somma":
+                    risultato = n1 + n2;
+                    break;
+                case "2":
+                case "sottrazione":
+                    risultato = n1 - n2;
+                    break;
+                case "3":
+                case"moltiplicazione":
+                    risultato = n1 * n2;
+                    break;
+                case "4":
+                case "divisione":
+                    risultato = n1 / n2;
+                    break;
+            }
+            return risultato;
+        }
+        
+           
+
+    }
+
+}
